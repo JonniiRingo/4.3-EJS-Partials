0b58b9e1a6d35aa575e4780ea2da4efc6677e40b
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-07-07 16:31:58 -0700

    FCK yeah dude I solved it with no help

--- index.js
+++ index.js
@@ -16,34 +16,19 @@ const port = 3000;
 app.set('view engine', 'ejs'); 
 
 
-
-// Using body parser so the backend can parse through 
-// different aspects of the html page essentially making each 
-// element into an object 
 app.use(express.static("public"));
 
-// Step 1: Render the home page "/" index.ejs
 app.get("/", (req, res) => {
   res.render("index.ejs"); 
 }); 
 
-// Step 2: Make sure that static files are linked to and the 
-// CSS shows up.
+app.get("/about", (req, res) => {
+  res.render("about.ejs"); 
+}); 
 
-
-
-
-
-// Step 3: Add the routes to handle the render of the about 
-// and contact pages.
-// Hint: Check the nav bar in the header.ejs to see the 
-// button hrefs
-
-
-// Step 4: Add the partials to the about and contact pages to
-//  show the header and footer on those pages. */
-
-
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs"); 
+}); 
 
 
 app.listen(port, () => {
