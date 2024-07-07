25b163419fe9cd328f8c95dcb64c80eca98b4049
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-07-07 15:59:00 -0700

    got the home page to render clear

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-import express from "express";
-
-const app = express();
-const port = 3000;
-
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
 Step 2: Make sure that static files are linked to and the CSS shows up.
 Step 3: Add the routes to handle the render of the about and contact pages.
   Hint: Check the nav bar in the header.ejs to see the button hrefs
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// setting the view engine to ejs
+app.set('view engine', 'ejs'); 
+
+
+
+// Using body parser so the backend can parse through 
+// different aspects of the html page essentially making each 
+// element into an object 
+app.use(express.static("public"));
+
+// Step 1: Render the home page "/" index.ejs
+app.get("/", (req, res) => {
+  res.render("index.ejs"); 
+}); 
+
+// Step 2: Make sure that static files are linked to and the 
+// CSS shows up.
+
+
+
+
+
+// Step 3: Add the routes to handle the render of the about 
+// and contact pages.
+// Hint: Check the nav bar in the header.ejs to see the 
+// button hrefs
+
+
+// Step 4: Add the partials to the about and contact pages to
+//  show the header and footer on those pages. */
+
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
